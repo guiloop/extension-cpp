152c8588f4b0175ab7907dfef4411ef8ac7318e2
guiloop <guiloop.com@gmail.com>
2025-02-09 15:16:27 +0000

    chore: add some file

---
+++ extension_cpp/csrc/module.cpp
@@ -0,0 +1,21 @@
+#include <torch/extension.h>
+#include "muladd.h"
+
+namespace extension_cpp {
+// Registers _C as a Python extension module.
+PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {}
+
+// Defines the operators
+TORCH_LIBRARY(extension_cpp, m) {
+  m.def("mymuladd(Tensor a, Tensor b, float c) -> Tensor");
+  m.def("mymul(Tensor a, Tensor b) -> Tensor");
+  m.def("myadd_out(Tensor a, Tensor b, Tensor(a!) out) -> ()");
+}
+
+// Registers CUDA implementations for mymuladd, mymul, myadd_out
+TORCH_LIBRARY_IMPL(extension_cpp, CPU, m) {
+  m.impl("mymuladd", &mymuladd_cpu);
+  m.impl("mymul", &mymul_cpu);
+  m.impl("myadd_out", &myadd_out_cpu);
+}
+}
--- extension_cpp/csrc/muladd.cpp
+++ extension_cpp/csrc/muladd.cpp
@@ -1,6 +1,5 @@
 #include <vector>
 #include <torch/torch.h>
-// #include <torch/extension.h>
 
 #include "muladd.h"
 
@@ -62,22 +61,5 @@ void myadd_out_cpu(const torch::Tensor& a, const torch::Tensor& b, torch::Tensor
     result_ptr[i] = a_ptr[i] + b_ptr[i];
   }
 }
-
-// Registers _C as a Python extension module.
-// PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {}
-
-// Defines the operators
-// TORCH_LIBRARY(extension_cpp, m) {
-//   m.def("mymuladd(Tensor a, Tensor b, float c) -> Tensor");
-//   m.def("mymul(Tensor a, Tensor b) -> Tensor");
-//   m.def("myadd_out(Tensor a, Tensor b, Tensor(a!) out) -> ()");
-// }
-
-// Registers CUDA implementations for mymuladd, mymul, myadd_out
-// TORCH_LIBRARY_IMPL(extension_cpp, CPU, m) {
-//   m.impl("mymuladd", &mymuladd_cpu);
-//   m.impl("mymul", &mymul_cpu);
-//   m.impl("myadd_out", &myadd_out_cpu);
-// }
 
 }
